69513be541517d4b009a192c30e478f023a8f43b
Sanya <wasd305@mail.ru>
2025-12-15 22:24:26 +0300

    Completed task NUMBER ONE

--- hm1-project/src/App.jsx
+++ hm1-project/src/App.jsx
@@ -1,37 +1,64 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from "./app.module.css";
+import moment from "moment"
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [value, setValue] = useState('');
+
+	const [list, setList] = useState([]);
+
+	const [error, setError] = useState('');
+
+	let isValueVaild = value.length >= 3;
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt();
+		if( promptValue.length < 3) {
+			setError('Введенное значение должно содержать минимум 3 символа')
+		} else {
+			setValue(promptValue)
+			setError('')
+		}
+
+	}
+
+	const onAddButtonClick = () => {
+		const id = Date.now()
+		const date = moment().format('DD.MM.YY HH:mm:ss')
+		if (value.length >= 3) {
+			const updatedList = [...list, {id, value, date}]
+			setList(updatedList)
+			setError('')
+			setValue('')
+		}
+
+	}
+
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
+			<div className={styles.app}>
+				<h1 className={styles.pageHeading}>Ввод значения</h1>
+				<p className={styles.noMarginText}>
+					Текущее значение <code>value</code>: "<output className={styles.currentValue}>{value}</output>"
 				</p>
+				{ error !== '' && <div className={styles.error}>
+					{error}
+				</div>}
+				<div className={styles.buttonsContainer}>
+					<button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
+					<button className={styles.button} disabled={!isValueVaild} onClick={onAddButtonClick}>
+						Добавить в список
+					</button>
+				</div>
+				<div className={styles.listContainer}>
+					<h2 className={styles.listHeading}>Список:</h2>
+					{ list.length === 0  ? <p className={styles.noMarginText}>Нет добавленных элементов</p> : <ul className={styles.list}>
+						{list.map(({id, value, date}) => (<li className={styles.listItem} key={id}>{value} | {date}</li>))}
+					</ul>
+					}
+				</div>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
