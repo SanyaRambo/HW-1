315a05ccdbe3a6f78059c5fd6bb60a5c9a5095f2
Sanya <wasd305@mail.ru>
2025-12-16 12:40:25 +0300

    adding editor prettier and ESLint 2.0

--- hm1-project/src/App.jsx
+++ hm1-project/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import styles from "./app.module.css";
-import moment from "moment"
+import { useState } from 'react';
+import styles from './app.module.css';
+import moment from 'moment';
 
 function App() {
 	const [value, setValue] = useState('');
@@ -13,50 +13,59 @@ function App() {
 
 	const onInputButtonClick = () => {
 		const promptValue = prompt();
-		if( promptValue.length < 3) {
-			setError('Введенное значение должно содержать минимум 3 символа')
+		if (promptValue.length < 3) {
+			setError('Введенное значение должно содержать минимум 3 символа');
 		} else {
-			setValue(promptValue)
-			setError('')
+			setValue(promptValue);
+			setError('');
 		}
-
-	}
+	};
 
 	const onAddButtonClick = () => {
-		const id = Date.now()
-		const date = moment().format('DD.MM.YY HH:mm:ss')
+		const id = Date.now();
+		const date = moment().format('DD.MM.YY HH:mm:ss');
 		if (value.length >= 3) {
-			const updatedList = [...list, {id, value, date}]
-			setList(updatedList)
-			setError('')
-			setValue('')
+			const updatedList = [...list, { id, value, date }];
+			setList(updatedList);
+			setError('');
+			setValue('');
 		}
-
-	}
-
+	};
 
 	return (
 		<>
 			<div className={styles.app}>
 				<h1 className={styles.pageHeading}>Ввод значения</h1>
 				<p className={styles.noMarginText}>
-					Текущее значение <code>value</code>: "<output className={styles.currentValue}>{value}</output>"
+					Текущее значение <code>value</code>: "
+					<output className={styles.currentValue}>{value}</output>"
 				</p>
-				{ error !== '' && <div className={styles.error}>
-					{error}
-				</div>}
+				{error !== '' && <div className={styles.error}>{error}</div>}
 				<div className={styles.buttonsContainer}>
-					<button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
-					<button className={styles.button} disabled={!isValueVaild} onClick={onAddButtonClick}>
+					<button className={styles.button} onClick={onInputButtonClick}>
+						Ввести новое
+					</button>
+					<button
+						className={styles.button}
+						disabled={!isValueVaild}
+						onClick={onAddButtonClick}
+					>
 						Добавить в список
 					</button>
 				</div>
 				<div className={styles.listContainer}>
 					<h2 className={styles.listHeading}>Список:</h2>
-					{ list.length === 0  ? <p className={styles.noMarginText}>Нет добавленных элементов</p> : <ul className={styles.list}>
-						{list.map(({id, value, date}) => (<li className={styles.listItem} key={id}>{value} | {date}</li>))}
-					</ul>
-					}
+					{list.length === 0 ? (
+						<p className={styles.noMarginText}>Нет добавленных элементов</p>
+					) : (
+						<ul className={styles.list}>
+							{list.map(({ id, value, date }) => (
+								<li className={styles.listItem} key={id}>
+									{value} | {date}
+								</li>
+							))}
+						</ul>
+					)}
 				</div>
 			</div>
 		</>
