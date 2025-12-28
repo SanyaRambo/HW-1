7ab6036cc78abaaac3647e19f0725d1f31afd482
Sanya <wasd305@mail.ru>
2025-12-28 15:21:48 +0300

    calculator full ready 2.0

--- hm1-project/src/App.jsx
+++ hm1-project/src/App.jsx
@@ -5,29 +5,23 @@ function App() {
 	const [operand1, setOperand1] = useState('0');
 	const [operator, setOperator] = useState('');
 	const [operand2, setOperand2] = useState('');
-	const [flag, setFlag] = useState(false)
+	const [flag, setFlag] = useState(false);
 
 	const output = operand1 + operator + operand2;
 
-	const nums = [
-		'7', '8', '9',
-		'4', '5', '6',
-		'1','2', '3',
-		'0'];
+	const nums = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 	const operatBtn = ['+', '-', '=', 'С'];
-
-
 
 	const hundleOperandButton = (btnValue) => {
 		const numberButton = btnValue;
 
-		if(operator === ''){
+		if (operator === '') {
 			if (operand1 === '0') {
-			setOperand1(numberButton);
-		} else {
-			setOperand1(operand1 + numberButton);
-		}
-		} else if(operand2 === '0') {
+				setOperand1(numberButton);
+			} else {
+				setOperand1(operand1 + numberButton);
+			}
+		} else if (operand2 === '0') {
 			setOperand2(numberButton);
 		} else {
 			setOperand2(operand2 + numberButton);
@@ -35,56 +29,77 @@ function App() {
 	};
 
 	const hundleOperator = (btnValue) => {
-
 		const operatorButton = btnValue;
 
 		if (operand1 !== '' && operatorButton !== '=') {
-			setOperator(operatorButton)
+			setOperator(operatorButton);
 		}
-
 
 		if (operatorButton === 'С' && operand1 !== '') {
-			setOperand1('0')
-			setOperand2('')
-			setOperator('')
+			setOperand1('0');
+			setOperand2('');
+			setOperator('');
 		}
 
-		if (operand1 !== '' && operatorButton === '=' && operand2 !== '' && operator === '+') {
-			setOperator(operator)
-			setOperand1(Number(operand1) + Number(operand2))
-			setOperand2('')
-			setOperator('')
-			setFlag(true)
-		} else if (operand1 !== '' && operatorButton === '=' && operand2 !== '' && operator === '-') {
-			setOperator(operator)
-			setOperand1(Number(operand1) - Number(operand2))
-			setOperand2('')
-			setOperator('')
-			setFlag(true)
+		if (
+			operand1 !== '' &&
+			operatorButton === '=' &&
+			operand2 !== '' &&
+			operator === '+'
+		) {
+			setOperator(operator);
+			setOperand1(Number(operand1) + Number(operand2));
+			setOperand2('');
+			setOperator('');
+			setFlag(true);
+		} else if (
+			operand1 !== '' &&
+			operatorButton === '=' &&
+			operand2 !== '' &&
+			operator === '-'
+		) {
+			setOperator(operator);
+			setOperand1(Number(operand1) - Number(operand2));
+			setOperand2('');
+			setOperator('');
+			setFlag(true);
 		} else {
-			setFlag(false)
+			setFlag(false);
 		}
 
-		console.log('Временный знак', operatorButton, 'Состояние', operator)
-
-	}
-
+		console.log('Временный знак', operatorButton, 'Состояние', operator);
+	};
 
 	return (
 		<>
 			<h1>CALCULATOR</h1>
-			<div className={`${flag ? styles.expressionResult : styles.expressionNotResult} ${styles.expression} `}>
+			<div
+				className={`${flag ? styles.expressionResult : styles.expressionNotResult} ${styles.expression} `}
+			>
 				<output>{output}</output>
 			</div>
 			<div className={styles.calc}>
 				<div className={styles.calculatorNum}>
 					{nums.map((btn) => (
-						<button onClick={() => hundleOperandButton(btn)} value={btn} key={btn}>{btn}</button>
+						<button
+							onClick={() => hundleOperandButton(btn)}
+							value={btn}
+							key={btn}
+						>
+							{btn}
+						</button>
 					))}
 				</div>
 				<div className={styles.calculatorOp}>
 					{operatBtn.map((opBtn) => (
-						<button className={styles.opAndRes} onClick={() => hundleOperator(opBtn)} value={opBtn} key={opBtn+0}>{opBtn}</button>
+						<button
+							className={styles.opAndRes}
+							onClick={() => hundleOperator(opBtn)}
+							value={opBtn}
+							key={opBtn + 0}
+						>
+							{opBtn}
+						</button>
 					))}
 				</div>
 			</div>
