f920312fdf76e06693107d03d7d55ea77f47b21f
Sanya <wasd305@mail.ru>
2026-01-12 16:54:15 +0300

    Stateles and Stateful

--- hm1-project/src/App.jsx
+++ hm1-project/src/App.jsx
@@ -1,12 +1,50 @@
 import PropTypes from "prop-types";
+import { useState } from "react"
 import "./App.css";
 
 
-const Product = ({ name, price }) => {
-	return <div>{name} - {price} руб</div>
+const Stateless = ({a, b, setA, setB, sum}) => {
+	return (
+		<>
+		<div>
+			Сумма a: {a}
+		</div>
+		<button onClick={() => (setA(a + 1))}>Прибавить к "a:{a}" +1</button>
+		<div>
+			Сумма b: {b}
+		</div>
+		<button onClick={() => (setB(b + 1))}>Прибавить к "b:{b}" +1</button>
+		<div>
+			Сумма a и b: {sum}
+		</div>
+		</>
+	)
+}
+
+
+
+export const App = () => {
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(0);
+
+	const sum = a + b
+
+	console.log(sum)
+
+	return (
+		<>
+			<Stateless a={a} setA={setA} b={b} setB={setB} sum={sum}/>
+		</>
+	)
 };
 
-Product.propTypes = {
-	name: PropTypes.string,
-	price: PropTypes.number,
+Stateless.propTypes = {
+	sum: PropTypes.string,
+	a: PropTypes.number,
+	b: PropTypes.number,
+	setA: PropTypes.number,
+	setB: PropTypes.number,
 };
+
+
+
