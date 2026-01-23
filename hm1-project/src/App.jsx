0a58c6f197f289b9a112f00bc78a278285252e22
Sanya <wasd305@mail.ru>
2026-01-23 17:19:38 +0300

    hw1 Forms

--- hm1-project/src/App.jsx
+++ hm1-project/src/App.jsx
@@ -1,21 +1,13 @@
-import PropTypes from "prop-types";
-import { useState } from "react"
-import styles from "./app.module.css";
 
 
 const App = () => {
 
-	const [value, setValue] = useState('')
-
 	return (
 		<>
-		<h1>Контролируемые и неконтролируемые поля</h1>
-		<div>
-			<input type="text" value={value} onChange={({target}) => setValue(target.value)} className={styles.inputOne} />
-		</div>
+
+
 		</>
-	)
+	);
 };
 
-export default App
-
+export default App;
